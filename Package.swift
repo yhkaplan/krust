0c732863569a66885c108635c348614d23242974
Joshua Kaplan <yhkaplan@gmail.com>
2025-01-29 13:46:35 -0800

    Implement basic CLI

--- Package.swift
+++ Package.swift
@@ -5,10 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "krust",
+    platforms: [.macOS(.v15)],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "krust"),
+        .executableTarget(name: "krust"),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,46 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
 
-print("Hello, world!")
+do {
+    try main()
+} catch {
+    print("Error: \(error.localizedDescription)")
+    exit(1)
+}
+
+enum KrustError: Error {
+    case invalidArguments
+}
+
+func main() throws {
+    let arguments = CommandLine.arguments
+
+    switch arguments.count {
+    case 1:
+        try runPrompt()
+    case 2:
+        let script = try readFile(arguments[1])
+        try run(script)
+    default:
+        throw KrustError.invalidArguments
+    }
+}
+
+func runPrompt() throws {
+    prompt: while true {
+        print("> ", terminator: "")
+        guard let line = readLine() else { break prompt }
+        switch line {
+        case ":exit\n", "e:", ":quit", ":q":
+            break prompt
+        default:
+            try run(line)
+        }
+    }
+}
+
+func readFile(_ path: String) throws -> String {
+    let url = URL(filePath: path)
+    return try String(contentsOf: url, encoding: .utf8)
+}
+
+func run(_ script: String) throws {}
